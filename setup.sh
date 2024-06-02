18203692eb49b80e521cb74917ef41c63a1863a7
kami <prashannasthapit@outlook.com>
2024-06-02 20:29:10 +0545

    update 3

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cd
 pacman -Syu
 pacman -S --noconfirm gnome firefox git base-devel xmlsec fuse2 icu iproute2 libdnet libmspack libsigc++ libxcrypt libcrypt.so libxss lsb-release procps-ng uriparser gdk-pixbuf-xlib chrpath doxygen gtkmm3 libxtst python rpcsvc-proto netctl cunit
 systemctl enable gdm.service
@@ -14,32 +13,17 @@ make
 make check
 make install
 ldconfig
-cd
 
 # Set up VMware Tools services
-cd setup-arch
+cd ..
+cd ..
 cp ./vmtoolsd.service /usr/lib/systemd/system/vmtoolsd.service
 cp ./vmware-vmblock-fuse.service /usr/lib/systemd/system/vmware-vmblock-fuse.service
-cd
 
 systemctl enable vmtoolsd.service
 systemctl start vmtoolsd.service
 systemctl enable vmware-vmblock-fuse.service
 systemctl start vmware-vmblock-fuse.service
 
-
 # Clean up
-pacman -R --noconfirm chrpath doxygen rpcsvc-proto cunit
-rm -rf open-vm-tools
-
-# add user
-username="kami"
-
-sudo usermod -aG wheel $username
-
-# Set password for the user
-read -s -p "Enter password for $username: " password
-echo "$username:$password" | sudo chpasswd
-
-# Activate wheel group in sudoers
-sudo sed -i '/%wheel ALL=(ALL) ALL/s/^# //' /etc/sudoers
+# pacman -R --noconfirm chrpath doxygen rpcsvc-proto cunit
