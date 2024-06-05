0a12037a6ae23b77c5bc0eed80d59958f1744309
kami <prashannasthapit@outlook.com>
2024-06-05 16:10:12 +0545

    file removed

--- gdm-scaling.sh
+++
@@ -1,4 +0,0 @@
-cp ~/.config/monitors.xml ~gdm/.config/monitors.xml
-chown gdm:gdm ~gdm/.config/monitors.xml
-machinectl shell gdm@ /bin/bash
-gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
